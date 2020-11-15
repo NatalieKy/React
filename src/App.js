1e2d52968c970db408ff6956d64de27654062d48
NatalieKy <nataliekyryk1@gmail.com>
2020-11-15 21:46:34 +0200

    cleaned project

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
 import './App.css';
-import React from "react";
-import Users from "./components/Users";
-import Comments from "./components/Comments";
-import Posts from "./components/Posts";
-import {NavLink, Route, Switch, BrowserRouter as Router} from "react-router-dom";
-import User from "./components/User";
-import '../src/components/styles.css'
-
 
 
 function App() {
     return (
-            <Router>
-                <div className={"d-flex justify-content-center"}>
-                    <NavLink className={"p-2"} to='/'>Home</NavLink>
-                    <NavLink className={"p-2"} to='/users'>Users</NavLink>
-                </div>
-                <div>
-                    <Switch>
-                        <Route path='/users/:id/posts' component={Posts}/>
-                        <Route path='/users' component={Users}/>
-                        <Route path='/' render={() => {return <h1 className={"text-center"}>Hello, this is homepage!</h1>}}/>
-                    </Switch>
-                </div>
-            </Router>
+            <div></div>
     );
 }
 export default App;
